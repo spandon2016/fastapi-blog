0f915d2472a1226b28d5afc704ade4a8c1cbf970
spandon2016 <spandon@gmail.com>
2024-02-19 14:23:56 -0500

    Initial fast api blog

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from database import models
+from database.database import engine
+from routers import post
+
+
+app = FastAPI()
+
+app.include_router(post.router)
+
+models.Base.metadata.create_all(engine)
---
+++ routers/post.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from routers.schemas import PostBase, PostDisplay
+from sqlalchemy.orm import Session 
+from database.database import get_db
+from database import db_post
+from routers.schemas import PostBase
+
+
+router = APIRouter(
+    prefix='/post',
+    tags=['post']
+
+)
+
+@router.post('')
+def create(request: PostBase, db: Session = Depends(get_db)):
+    return db_post.create(db, request)
+
+@router.get('/all')
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
+
+@router.delete('/{id}')
+def delete(id: int, db: Session = Depends(get_db)):
+    return db_post.delete(id, db)
+    
